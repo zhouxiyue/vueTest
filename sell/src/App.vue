e71a5a693178a16f08c83565db8bdd8ede06c8b6
zhouxiyue <15810683906@163.com>
2021-09-02 19:16:43 +0800

    add div

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
     <!--<img src="./assets/logo.png">-->
-    <router-view/>
+    <!--<router-view/>-->
+    <div class="header">
+      I am header!
+    </div>
+    <div class="tab">
+      I am tab!
+    </div>
+    <div class="content">
+      I am content!
+    </div>
   </div>
 </template>
 
