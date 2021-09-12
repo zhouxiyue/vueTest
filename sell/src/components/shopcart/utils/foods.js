24bd1c29fd54d72e682cd74f20b67ce55c9cbaea
zhouxiyue <15810683906@163.com>
2021-09-12 20:34:26 +0800

    add test function

--- sell/src/components/shopcart/utils/foods.js
+++ sell/src/components/shopcart/utils/foods.js
@@ -1,3 +1,11 @@
 export const getTotalPrice = (foods) => {
   return foods.reduce((prev, food) => prev + food.price * food.count, 0);
-}
+};
+
+export const test1 = (foods) => {
+  return 'test1';
+};
+
+export const test2 = (foods) => {
+  return 'test2';
+};
