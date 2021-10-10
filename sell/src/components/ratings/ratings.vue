2f60482a76b2243714854a5eaf299457ef232714
zhouxiyue <15810683906@163.com>
2021-10-10 11:47:57 +0800

    add ratingselect

--- sell/src/components/ratings/ratings.vue
+++ sell/src/components/ratings/ratings.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="content">I am ratings</div>
+  <div class="ratings">
+    <div class="ratings-content">
+
+    </div>
+  </div>
 </template>
 
 <script>
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  .ratings
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
 </style>
--- sell/src/components/ratingselect/ratingselect.vue
+++ sell/src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="ratingselect">
-    <div class="rating-type">
-      <span class="block positive" :class="{'active':selectType===2}">{{desc.all}}<span class="count">47</span></span>
-      <span class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span class="count">48</span></span>
-      <span class="block negative" :class="{'active':selectType===1}">{{desc.negative}}<span class="count">49</span></span>
+    <div class="rating-type border-1px">
+      <span @click="select(2,$event)" class="block positive"
+            :class="{'active':selectType===2}">{{desc.all}}<span
+        class="count">{{ratings.length}}</span></span>
+      <span @click="select(0,$event)" class="block positive"
+            :class="{'active':selectType===0}">{{desc.positive}}<span
+        class="count">{{positives.length}}</span></span>
+      <span @click="select(1,$event)" class="block negative"
+            :class="{'active':selectType===1}">{{desc.negative}}<span
+        class="count">{{negatives.length}}</span></span>
     </div>
-    <div class="switch" :class="{'on':onlyContent}">
+    <div @click="toggleContent" class="switch" :class="{'on':onlyContent}">
       <span class="icon-check_circle"></span>
       <span class="text">只看有内容的评价</span>
     </div>
@@ -13,8 +19,8 @@
 </template>
 
 <script type="text/ecmascript-6">
-  // const POSITIVE = 0;
-  // const NEGATIVE = 1;
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
   const ALL = 2;
   export default {
     props: {
@@ -41,6 +47,34 @@
             negative: '不满意'
           };
         }
+      }
+    },
+    computed: {
+      positives() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === POSITIVE;
+        });
+      },
+      negatives() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === NEGATIVE;
+        });
+      }
+    },
+    methods: {
+      select(type, event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.selectType = type;
+        this.$emit('select', type);
+      },
+      toggleContent(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.onlyContent = !this.onlyContent;
+        this.$emit('toggle', this.onlyContent);
       }
     }
   };
@@ -81,13 +115,16 @@
       border-bottom: 1px solid rgba(7,17,27,0.1)
       color: rgb(147,153,159)
       font-size: 24px
-    .icon-check_circle
-      display: inline-block
-      vertical-align : top
-      margin-right: 4px
-      font-size: 24px
-    .text
-      display: inline-block
-      vertical-align: top
-      font-size: 12px
+      &.on
+        .icon-check_circle
+          color: #00c850
+      .icon-check_circle
+        display: inline-block
+        vertical-align : top
+        margin-right: 4px
+        font-size: 24px
+      .text
+        display: inline-block
+        vertical-align: top
+        font-size: 12px
 </style>
